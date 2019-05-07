af38f2aead40fea6a8e947c18768a1577094875a
Amir <seyedhesamian@northwesternmutual.com>
2019-05-06 22:22:44 -0500

    wip

--- src/app/constants/api.ts
+++ src/app/constants/api.ts
@@ -2,8 +2,14 @@ import {environment} from '../../environments/environment';
 
 const PORT = 5000;
 const HOST = 'localhost';
+let BASE_ADDRESS = '';
 
-const BASE_ADDRESS = !environment.production ? `http://${HOST}:${PORT}` : '';
+if (environment.production) {
+  const BASE_ADDRESS = 'https://dotnet-workshop-backend.herokuapp.com';
+} else {
+  const BASE_ADDRESS = `http://${HOST}:${PORT}`;
+}
+
 const API_ADDRESS = `${BASE_ADDRESS}/api`;
 
 export { API_ADDRESS, BASE_ADDRESS};
