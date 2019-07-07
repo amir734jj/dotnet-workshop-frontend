2d688fa3a84174b0ef519bc5404676f2b7aa05ed
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2019-07-07 13:56:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,5 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/dotnet-intermediate-workshop'));
 
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname,'/dist/angular-tour-of-heroes/index.html'));
-});
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 80);
